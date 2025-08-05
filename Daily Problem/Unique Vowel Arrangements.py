0616bc027cadbfb1a9df68640839318d75eb0411
OmKale000 <156779265+OmKale000@users.noreply.github.com>
2025-08-05 22:09:03 +0530

    Unique Vowel Arrangements

---
+++ Daily Problem/Unique Vowel Arrangements.py
@@ -0,0 +1,29 @@
+def fact(n):
+    res = 1
+    
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+def vowel_count(s):
+    freq = {}
+    vowels = 'aeiou'
+    
+    for c in s:
+        if c in vowels:
+            freq[c] = freq.get(c, 0) + 1
+    if not freq:
+        return 0
+    choices = 1
+    
+    for count in freq.values():
+        choices *= count
+    dist = len(freq)
+    
+    res = choices * fact(dist)
+    return res
+
+
+s = 'aaxyze'
+result = vowel_count(s)
+print(result)
