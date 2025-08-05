2406b3d4feacb32080284360d865fc9766f94c39
OmKale000 <156779265+OmKale000@users.noreply.github.com>
2025-08-05 22:08:44 +0530

    Number of co-prime pairs in an array

---
+++ Daily Problem/Number of co-prime pairs in an array.py
@@ -0,0 +1,39 @@
+def computeMobius(n, mu):
+    is_prime = [1] * (n + 1)
+    mu[0] = 0
+    mu[1] = 1
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            for j in range(i, n + 1, i):
+                mu[j] *= -1
+                is_prime[j] = 0
+            for j in range(i * i, n + 1, i * i):
+                mu[j] = 0
+
+def buildFre(arr, freq):
+    for x in arr:
+        freq[x] += 1
+
+def computeDivCnt(maxVal, freq, d):
+    for k in range(1, maxVal + 1):
+        for j in range(k, maxVal + 1, k):
+            d[k] += freq[j]
+
+def cntCoprime(arr):
+    maxVal = max(arr)
+    freq = [0] * (maxVal + 1)
+    d = [0] * (maxVal + 1)
+    mu = [1] * (maxVal + 1)
+    buildFre(arr, freq)
+    computeDivCnt(maxVal, freq, d)
+    computeMobius(maxVal, mu)
+    result = 0
+    for k in range(1, maxVal + 1):
+        if mu[k] == 0 or d[k] < 2:
+            continue
+        pairs = d[k] * (d[k] - 1) // 2
+        result += mu[k] * pairs
+    return result
+
+arr = [4, 8, 3, 9]
+print(cntCoprime(arr))
