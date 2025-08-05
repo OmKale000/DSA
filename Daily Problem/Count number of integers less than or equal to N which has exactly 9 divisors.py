eb50ba6c1d1c778f2c2975e9c739ba7d95c1d727
OmKale000 <156779265+OmKale000@users.noreply.github.com>
2025-08-05 22:08:20 +0530

    Count number of integers less than or equal to N which has exactly 9 divisors

--- Daily Problem/Count number of integers less than or equal to N which has exactly 9 divisors.py
+++ Daily Problem/Count number of integers less than or equal to N which has exactly 9 divisors.py
@@ -6,7 +6,6 @@ def countNumbers(n):
 
     prime = [i for i in range(limit + 1)]
 
-    # Sieve to store smallest prime factor
     for i in range(2, int(math.sqrt(limit)) + 1):
         if prime[i] == i:
             for j in range(i * i, limit + 1, i):
@@ -17,10 +16,8 @@ def countNumbers(n):
         p = prime[i]
         q = prime[i // prime[i]]
 
-        # Check for p^2 * q^2 form
         if p * q == i and q != 1 and p != q:
             c += 1
-        # Check for p^8 form
         elif prime[i] == i and pow(i, 8) <= n:
             c += 1
 
